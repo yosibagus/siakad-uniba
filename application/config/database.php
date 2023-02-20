3dbb2ee808b33069239f23b2399ca0f711d67e1c
Yosi Bagus <yosibagusdsd@gmail.com>
2023-02-20 15:59:33 +0700

    update connection db

--- application/config/database.php
+++ application/config/database.php
@@ -5,35 +5,12 @@ $active_group = 'default';
 $query_builder = TRUE;
 
 
-$db['default'] = array(
-	'dsn'	=> '',
-	'hostname' => 'unibamadura.ac.id',
-	'username' => 'u5647534_admin',
-	'password' => 'Adminunibamadura20',
-	'database' => 'u5647534_uniba_siakad',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
-
-
 // $db['default'] = array(
 // 	'dsn'	=> '',
-// 	'hostname' => 'localhost',
-// 	'username' => 'root',
-// 	'password' => '',
-// 	'database' => 'uniba_siakad',
+// 	'hostname' => 'unibamadura.ac.id',
+// 	'username' => 'u5647534_admin',
+// 	'password' => 'Adminunibamadura20',
+// 	'database' => 'u5647534_uniba_siakad',
 // 	'dbdriver' => 'mysqli',
 // 	'dbprefix' => '',
 // 	'pconnect' => FALSE,
@@ -49,3 +26,26 @@ $db['default'] = array(
 // 	'failover' => array(),
 // 	'save_queries' => TRUE
 // );
+
+
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'root',
+	'password' => '',
+	'database' => 'uniba_siakad',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
